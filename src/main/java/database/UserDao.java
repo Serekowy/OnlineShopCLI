927b49d58b0499d3d64620f85aab8d71916d2a8d
Cheesy2137 <adrian997112999@gmail.com>
2026-05-22 20:24:09 +0200

    Add two methods to check if email exists and to get all users

--- src/main/java/database/UserDao.java
+++ src/main/java/database/UserDao.java
@@ -6,6 +6,8 @@ import model.Role;
 import model.User;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDao {
     private static final String DB_URL = "jdbc:sqlite:shop.db";
@@ -34,29 +36,66 @@ public class UserDao {
             pstmt.setString(1, username);
             ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
-                int id = rs.getInt("id");
-                String dbUsername = rs.getString("username");
-                String password = rs.getString("password");
-                String email = rs.getString("email");
-                String role = rs.getString("role");
-
-                User user;
-
-                if(role.equalsIgnoreCase("ADMIN")) {
-                    user = new Admin(dbUsername, password, email, Role.ADMIN);
-                } else {
-                    user = new NormalUser(dbUsername, password, email, Role.USER);
-                }
-
-                user.setId(id);
-                return user;
+                return getUserData(rs);
             }
 
             return null;
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean emailExists(String email) {
+        String sql = "SELECT 1 FROM users WHERE email = ?";
+
+        try(Connection conn = DriverManager.getConnection(DB_URL);
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, email);
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+
+        try(Connection conn = DriverManager.getConnection(DB_URL);
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                users.add(getUserData(rs));
+            }
+
+            return users;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private User getUserData(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String dbUsername = rs.getString("username");
+        String password = rs.getString("password");
+        String email = rs.getString("email");
+        String role = rs.getString("role");
+
+        User user;
+
+        if(role.equalsIgnoreCase("ADMIN")) {
+            user = new Admin(dbUsername, password, email, Role.ADMIN);
+        } else {
+            user = new NormalUser(dbUsername, password, email, Role.USER);
+        }
+
+        user.setId(id);
+        return user;
     }
 
 }
