35dad6450acb36c3de004762097b1e334979f1b9
Cheesy2137 <adrian997112999@gmail.com>
2026-06-01 19:26:19 +0200

    Add getters and setters

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -8,4 +8,44 @@ public class Product {
     private BigDecimal price;
     private int stockQuantity;
     private Category category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
